8d53e1e97b1f6e9d810e5a890e8d3d077a474255
LukaszMajkut <majkut.lukasz95@gmail.com>
2019-10-29 21:52:30 +0100

    Walczymy z tematem

--- Zadanie_2.4.py
+++ Zadanie_2.4.py
@@ -1,4 +1,5 @@
 
+#Tutaj próba możliwości wpisania liczb w jednej linijce...
 
 pula_liczb = []
 liczby = input("Podaj liczby które chciałbyś dodać do puli - oddziel je przecinkami: ") #możliwość dodania liczb jedynie z zakresu 0 - 9
@@ -6,6 +7,13 @@ for i in range(0,len(liczby),2):
     pula_liczb.append(int(liczby[i]))
 
 print (pula_liczb)
+
+# INACZEJ - najpierw określić ile liczb chce się wprowadzić..
+
+pula_liczb = []
+ilosc_liczb = int(input("Podaj ile liczb chcialbys wprowadzic: "))
+while len(pula_liczb) < ilosc_liczb:
+    pula_liczb.append(int(input("Podaj liczbę którą chciałbyś dodać do puli: ")))
 
 print (pula_liczb)
 print (len(pula_liczb))
@@ -15,7 +23,7 @@ print(min(pula_liczb))
 print(max(pula_liczb))
 
 
-#### póki co "na piechote" ze stworzeniem listy z n liczbami
+#### "na piechote" ze stworzeniem listy z n liczbami
 
 n = [2, 34, 55, 11, 200, 223, 45]
 
