56838057ca9c8d96ff163d225eb8f9928e98f522
LukaszMajkut <majkut.lukasz95@gmail.com>
2020-01-06 20:18:30 +0100

    stdlib

---
+++ Zad_6.py
@@ -0,0 +1,33 @@
+"""
+Napisz program wyświetlający pogodę dla wskazanej przez użytkownika lokalizacji.
+Skorzystaj z modułu urllib.request, json oraz API MetaWeather.
+"""
+
+import json
+import urllib.request
+from urllib.request import Request
+
+lokalizacja = input("Wpisz lokalizację dla której chciałbyś poznać pogode: ")
+url = f'https://www.metaweather.com/api/location/search/?query={lokalizacja}'
+
+zapytanie = Request(url)
+
+with urllib.request.urlopen(zapytanie) as req:
+    print(req.geturl())
+    print(req.info())
+    print(req.getcode())
+
+    zawartosc = json.loads(req.read())
+    print (zawartosc['title']['location_type']['woeid'])
+    city_id = zawartosc['woeid']
+    url2 = f'https://www.metaweather.com/api/location/{city_id}'
+
+    zapytanie2 = Request(url2)
+
+    with urllib.request.urlopen(zapytanie2) as req2:
+        print(req2.geturl())
+        print(req2.info())
+        print(req2.getcode())
+
+        zawartosc2  = json.loads(req2.read())
+        print (f'Temperatura w miejscie o nazwie {zawartosc["title"]} wynosi {zawartosc2["the_temp"]}')
